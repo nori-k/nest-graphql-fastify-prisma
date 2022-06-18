44ffe45021ddaa8976552bf4db71312aaa39bd2c
nori-k <norikatsu.kamiya@gmail.com>
2022-06-19 04:36:04 +0900

    foo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      graphiql: process.env.NODE_ENV !== 'production',
+      autoSchemaFile: join(process.cwd(), 'src/generated/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
